f04a3e31d7a885e4a7c39f34321caea59cc3784b
zwh <1466625029@qq.com>
2019-07-31 19:38:49 +0800

    Eureka加密

--- eurakeServer/src/main/java/com/example/server/EurekaServerApplication.java
+++ eurakeServer/src/main/java/com/example/server/EurekaServerApplication.java
@@ -13,7 +13,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  **/
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServerApplication {
+public class  EurekaServerApplication {
 
     public static void main(String[] args){
         SpringApplication.run(EurekaServerApplication.class);
